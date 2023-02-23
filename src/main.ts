34f4ac0aabecbf5f7e3f81671051905ad13c0d3a
Andy Li <andylilfs@gmail.com>
2023-02-23 13:47:06 -0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ?? 3000;
+  console.log(`Server started at port ${port}`);
+  await app.listen(port);
+}
+bootstrap();
