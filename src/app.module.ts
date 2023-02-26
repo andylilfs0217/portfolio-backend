c8c6a5431976065954a2f353aa7b513a601f57ec
Andy Li <andylilfs@gmail.com>
2023-02-26 15:22:07 -0500

    add mysql docker container

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,24 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { getEnvPath } from './common/helper/env.helper';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
     GithubModule,
   ],
   controllers: [AppController],
